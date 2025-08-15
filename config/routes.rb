962188cd2388deea149f392c5f33c612d95b1328
shimizu1023 <214320444+shimizu1023@users.noreply.github.com>
2025-08-16 00:11:39 +0900

    いいね機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
  
   resources :posts do
     resources :comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
   end
   get "up" => "rails/health#show", as: :rails_health_check
 
